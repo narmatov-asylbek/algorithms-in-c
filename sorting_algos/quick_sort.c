ffe99782b08e8d7163afbf6a25b1b732f6052602
asyl <narmatov.asylbek@gmail.com>
2022-05-15 14:00:09 +0600

    quick sort

--- sorting_algos/quick_sort.c
+++ sorting_algos/quick_sort.c
@@ -1,35 +1,33 @@
 #include "common.h"
-#include <limits.h>
 
 
 
-int partition(int A[], int l, int h) {
-    int pivot = A[l];
-    int i = l, j = h;
+int partition(int s[], int left, int right) {
+    int pivot, pos, i;
+    pivot = right;
+    pos = left;
 
-    do {
-        do {i++;} while (A[i] <= pivot);
-        do {j--;} while(A[j] >= pivot);
-
-        if (i < j) { swap(&A[i], &A[j]); }
-    } while (i < j) ;
-
-    swap(&A[l], &A[j]);
-    return j;
+    for (i = left; i < right; i++) {
+        if (s[i] < s[pivot]) {
+            swap(&s[i], &s[pos]);
+            pos++;
+        }
+    }
+    swap(&s[pivot], &s[pos]);
+    return pos;
 }
 
-void quickSort(int A[], int l, int h) {
-    int j;
-    if (l < h) {
-        j = partition(A, l, h);
-        quickSort(A, l, j);
-        quickSort(A, j + 1, h);
+void quickSort(int s[], int left, int right) {
+    int p;
+    if (left < right) {
+        p = partition(s, left, right);
+        quickSort(s, left, p - 1);
+        quickSort(s, p + 1, right);
     }
 }
 
-
 int main(void) {
-    int A[] = {20, 13, 23, 11, 99, 3, 7, 1, 34, 48, INT_MAX};
+    int A[] = {20, 13, 23, 11, 99, 3, 7, 1, 34, 48, 33};
     quickSort(A, 0, 10);
     display(A, 10);
     return 0;
